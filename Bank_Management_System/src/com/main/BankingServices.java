0c0932a281e735fbd9358a219a7db23c657ccbd5
Dell <Dell@DESKTOP-ND1EAOV>
2024-05-16 16:37:58 +0530

    Bank project by aboli

---
+++ Bank_Management_System/src/com/main/BankingServices.java
@@ -0,0 +1,14 @@
+package com.main;
+
+public class BankingServices 
+{
+	public void createAccount()
+	{
+		System.out.println("This is account creation");
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("welcome to banking management App");
+	}
+
+}
