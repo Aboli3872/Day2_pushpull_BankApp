0aa59b65922ecb4a88c72d75bba7c02fa3d98c89
Aboli3872 <168178760+Aboli3872@users.noreply.github.com>
2024-05-16 16:41:35 +0530

    added credit amount service

--- Bank_Management_System/src/com/main/BankingServices.java
+++ Bank_Management_System/src/com/main/BankingServices.java
@@ -6,6 +6,10 @@ public class BankingServices
 	{
 		System.out.println("This is account creation");
 	}
+	public void creditAmount()
+	{
+		System.out.println("This is amount credit service");
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("welcome to banking management App");
